f68af6f5adeff764a23372731f7ea4bef4b44d05
leorega <leonardoregazzoni@gmail.com>
2024-04-02 15:41:24 -0300

    Agrego base de datos Mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from "@nestjs/common";
+import { TasksModule } from "./tasks/tasks.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
+
+const connectionString = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.z8jtlkz.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`;
+
+//"mongodb://localhost/tasksdb"
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/tasksdb'), TasksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(connectionString),
+    TasksModule,
+  ],
 })
 export class AppModule {}
